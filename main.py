d214abfe17afbea9561b75c0329e5590a7177e15
annikam205 <annika.messer@web.de>
2021-04-23 10:38:47 +0200

    Get data for algorithm

---
+++ main.py
@@ -0,0 +1,15 @@
+# Assignment 4, Annika Messer
+# Link to GitHub https://github.com/annikam205/-Bio-Assignment-4
+
+import pandas as pd
+
+input_data = pd.read_csv('input.csv', sep=';', engine='python', skiprows=2, header=None)
+input_data.columns = ['X', 'Y']
+input_data = input_data.replace(',', '.', regex=True)
+
+x_axis = pd.to_numeric(input_data['X'], errors='raise', downcast=None)
+y_axis = pd.to_numeric(input_data['Y'], errors='raise', downcast=None)
+
+print(input_data)
+print(x_axis)
+print(y_axis)
